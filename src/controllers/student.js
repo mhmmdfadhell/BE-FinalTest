12326f82e39ab67dccdc6bd3e9bfc975ede9bd97
john doe <john@mail.com>
2023-06-07 17:37:21 +0700

    update terbaru

--- src/controllers/student.js
+++ src/controllers/student.js
@@ -3,16 +3,9 @@ const { user } = require("../../models");
 const joi = require("joi");
 const moment = require("moment");
 
-const getPagination = (page, size) => {
-  const limit = size ? +size : 4;
-  const offset = page ? page * limit : 0;
-  return { limit, offset };
-};
-
 exports.getAllStudent = async (req, res) => {
   try {
-    const { page, size, fullnameSort } = req.query;
-    const { limit, offset } = getPagination(page, size);
+
 
     const userData = await user.findOne({
       where: {
@@ -23,26 +16,17 @@ exports.getAllStudent = async (req, res) => {
       },
     });
 
-    const data = await student.findAndCountAll({
-      order: [["fullname", fullnameSort == 0 ? "ASC" : "DESC"]],
-      limit,
-      offset,
+    const data = await student.findAll({
       attrributes: {
         exclude: ["createdAt", "updatedAt"],
       },
     });
 
-    const { count: totalItems, rows: students } = data;
-    const currentPage = page ? +page : 0;
-    const totalPages = Math.ceil(totalItems / limit);
 
     return res.render("pages/dashboard", {
       users: userData,
       moment: moment,
-      students: students,
-      currentPage: currentPage,
-      totalPages: totalPages,
-      totalItems: totalItems,
+      students: data,
     });
   } catch (error) {
     console.log(error);
@@ -122,6 +106,64 @@ exports.editStudent = async (req, res) => {
     });
   }
 };
+
+exports.updateStudent = async (req,res) => {
+  try {
+    const body = req.body;
+    const id = req.params.id;
+    let age = moment().diff(moment(body.born_date, "YYYY-MM-DD"), "years");
+
+    const data = {
+      fullname: body.fullname,
+      age: age,
+      born_date: body.born_date,
+      grade: body.grade,
+      address: body.address,
+      hp: body.hp,
+      parent_name: body.parent_name,
+      parent_hp: body.parent_hp,
+    };
+
+    const schema = joi.object({
+      fullname: body.fullname,
+      born_date: body.born_date,
+      grade: body.grade,
+      address: body.address,
+      hp: body.hp,
+      parent_name: body.parent_name,
+      parent_hp: body.parent_hp,
+    });
+
+    const { error } = schema.validate(body);
+
+    if (error) {
+      return res.status(400).send({
+        message: error.details[0].message,
+      });
+    }
+
+    const findStudent = await student.findOne({
+      where: {
+        id: id,
+      },
+      attrributes: {
+        exclude: ["createdAt", "updatedAt"],
+      },
+    });
+
+    await findStudent.update(data, {
+      where:{
+        id
+      }
+    })
+    res.redirect("/dashboard")
+  } catch (error) {
+    console.log(error);
+    return res.status(500).send({
+      message: "Internal server error!",
+    });
+  } 
+}
 
 exports.deleteStudent = async (req, res) => {
   try {
--- src/routes/student.js
+++ src/routes/student.js
@@ -5,7 +5,7 @@ const { authToken } = require("../middleware/authToken");
 
 router.get("/dashboard", authToken, student.getAllStudent);
 router.post("/add-student", authToken, student.insertStudent);
-// router.post("/edit-student/:id", authToken, student.editStudent);
+router.post("/update-student/:id", authToken, student.updateStudent);
 router.get("/edit-student/:id", authToken, student.editStudent);
 router.get("/delete-student/:id", authToken, student.deleteStudent);
 
